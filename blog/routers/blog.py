3f3957e82560e814892869565fd175323fb81fa7
Nischal699 <nischal123321@gmail.com>
2025-06-05 13:09:11 +0545

    commited

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -16,17 +16,17 @@ def all(db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth
     return blog.get_all(db)
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.ShowBlog)
-def create(request : schemas.Blog, db: Session = Depends(get_db)):
+def create(request : schemas.Blog, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.create(request, db)
 
 @router.get("/{id}",status_code=200,response_model=schemas.ShowBlog)
-def show(id: int,response: Response, db: Session = Depends(get_db)):
+def show(id: int,response: Response, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.show(id, db)
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db: Session = Depends(get_db)):
+def destroy(id: int, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.destroy(id, db)
 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.update(id, request, db)
